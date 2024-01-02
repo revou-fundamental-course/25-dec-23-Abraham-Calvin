34c74c4cfbaec82f43403a60a13ab8ac57f0e1a8
Abraham-Calvin <abrahamcalvinpaju@gmail.com>
2024-01-03 01:33:43 +0800

    bug check

--- js/script.js
+++ js/script.js
@@ -75,3 +75,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button
+    var mybutton = document.getElementById("up-button");
+  
+    // When the user scrolls down 50px from the top of the document, show the button
+    window.onscroll = function() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    };
+  
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.onclick = function() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  });
